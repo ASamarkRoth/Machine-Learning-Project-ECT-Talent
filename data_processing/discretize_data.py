c4ddebb67127ade8ba735cf7630243e45bd2ace3
Anton Såmark-Roth <anton.samarkroth@gmail.com>
2020-08-24 13:19:06 +0200

    Removed unnecessary files.

--- data_processing/discretize_data.py
+++ data_processing/discretize_data.py
@@ -14,8 +14,9 @@ import pandas as pd
 import scipy as sp
 from sklearn.utils import shuffle
 
-from utils import data_discretization as dd
 import data_processing.generate_images as gi
+
+import math
 
 X_DISC = 20
 Y_DISC = 20
@@ -31,7 +32,7 @@ def generate_voxelised_data_set(data_dir, save_dir, prefix):
     raw_data = list(gi.read_and_label_data(data_dir).values())
     for i, l in enumerate(raw_data):
         xyzs = raw_data[i][0]
-        data.append([dd.discretize_grid_charge(xyzs, X_DISC, Y_DISC, Z_DISC), raw_data[i][1]])
+        data.append([discretize_grid_charge(xyzs, X_DISC, Y_DISC, Z_DISC), raw_data[i][1]])
 
     # Split into train and test sets
     data = shuffle(data)
@@ -57,6 +58,55 @@ def generate_voxelised_data_set(data_dir, save_dir, prefix):
     h5.create_dataset('train_targets', data=train_targets)
     h5.create_dataset('test_targets', data=test_targets)
     h5.close()
+
+DETECTOR_LENGTH = 1250.0
+DETECTOR_RADIUS = 275.0
+
+def discretize_grid_charge(xyz, x_disc, y_disc, z_disc):
+    """Discretizes AT-TPC point cloud data-processing using a grid geometry by totalling
+    charge of hits in a given rectangular bucket.
+
+    Parameters
+    ----------
+    xyz    : point cloud data-processing with shape (n,5) where n is the number of traces
+    x_disc : number of slices in x
+    y_disc : number of slices in y
+    z_disc : number of slices in z
+
+    Returns
+    -------
+    The discretized data-processing in a csr sparse matrix of shape (1, x_disc*y_disc*z_disc)
+    """
+
+    # calculate desired discreuniform_param_generatortization resolution
+    disc_elements = x_disc * y_disc * z_disc
+
+    buckets = []
+    charges = []
+
+    for point in xyz:
+        # check that z-coordinate of point is in appropriate range
+        if point[2] > DETECTOR_LENGTH:
+            continue
+
+        x_bucket = math.floor(((point[0] + DETECTOR_RADIUS) / (2 * DETECTOR_RADIUS)) * x_disc)
+        y_bucket = math.floor(((point[1] + DETECTOR_RADIUS) / (2 * DETECTOR_RADIUS)) * y_disc)
+        z_bucket = math.floor((point[2] / DETECTOR_LENGTH) * z_disc)
+
+        bucket_num = z_bucket * x_disc * y_disc + x_bucket + y_bucket * x_disc
+
+        buckets.append(bucket_num)
+
+        # scaling by factor of 1000
+        charges.append(point[3] / 1000)
+
+    cols = buckets
+    rows = np.zeros(len(cols))
+    data = charges
+
+    # automatically sums charge values for data-processing occuring at the (row, col)
+    discretized_data_sparse_CHARGE = sp.sparse.csr_matrix((data, (rows, cols)), shape=(1, disc_elements))
+    return discretized_data_sparse_CHARGE
 
 
 
