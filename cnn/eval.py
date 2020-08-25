1e702c72e59d2b7eee3327c05c894265bb9b7612
Anton Såmark-Roth <anton.samarkroth@gmail.com>
2020-08-25 15:41:45 +0200

    CNN eval with ConfusionMatrixDisplay.

--- cnn/eval.py
+++ cnn/eval.py
@@ -6,8 +6,13 @@ import click
 import numpy as np
 import tensorflow as tf
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import itertools
 
-from utils.data import load_image_h5, CLASS_NAMES
+from data_processing.data import CLASS_NAMES, load_image_h5
 
 FEATURES = 0
 TARGETS = 1
@@ -28,36 +33,36 @@ TARGETS = 1
 def main(model_file, data, data_combine, binary, examples_limit, seed, reverse_labels):
     eval(model_file, data, data_combine, binary, examples_limit, seed, reverse_labels)
     
-def eval(model_file, data, data_combine=False, binary=True, examples_limit=-1, seed=71, reverse_labels=False):
+def eval(model_file, data, name='CNN', data_combine=False, binary=True, examples_limit=-1, seed=71, reverse_labels=False):
     """This function will evaluate a CNN classifier.
 
     Loss, accuracy, and classification metrics are printed to the console.
     """
     assert model_file.endswith('.h5'), 'model_file must point to an HDF5 file'
-    assert data.endswith('.h5'), 'data must point to an HDF5 file'
+    #assert data.endswith('.h5'), 'data must point to an HDF5 file'
 
     # Set random seeds
     np.random.seed(seed)
     tf.random.set_seed(seed)
 
     # Load data
-    if data_combine:
-        a, b = load_image_h5(data, categorical=True, binary=binary, reverse_labels=reverse_labels)
-        test = np.concatenate([a[FEATURES], b[FEATURES]], axis=0), np.concatenate([a[TARGETS], b[TARGETS]], axis=0)
-    else:
-        _, test = load_image_h5(data, categorical=True, binary=binary, reverse_labels=reverse_labels)
+    #if data_combine:
+    #    a, b = load_image_h5(data, categorical=True, binary=binary, reverse_labels=reverse_labels)
+    #    test = np.concatenate([a[FEATURES], b[FEATURES]], axis=0), np.concatenate([a[TARGETS], b[TARGETS]], axis=0)
+    #else:
+    #    _, test = load_image_h5(data, categorical=True, binary=binary, reverse_labels=reverse_labels)
 
     if examples_limit == -1:
-        examples_limit = test[TARGETS].shape[0]
+        examples_limit = data[TARGETS].shape[0]
 
     # Load the model
     model = tf.keras.models.load_model(model_file)
 
     # Evaluate the model
-    loss, acc = model.evaluate(test[FEATURES][:examples_limit], test[TARGETS][:examples_limit])
+    loss, acc = model.evaluate(data[FEATURES][:examples_limit], data[TARGETS][:examples_limit])
 
     # Make predictions
-    preds = np.round(model.predict(test[FEATURES][:examples_limit]))
+    preds = np.round(model.predict(data[FEATURES][:examples_limit]))
     
     #if binary:
     #    if reverse_labels:
@@ -73,14 +78,55 @@ def eval(model_file, data, data_combine=False, binary=True, examples_limit=-1, s
         target_names = target_names[::-1]
 
     # Get classification metrics
-    report = classification_report(test[TARGETS][:examples_limit], preds, target_names=target_names, digits=2)
+    report = classification_report(data[TARGETS][:examples_limit], preds, target_names=target_names, digits=2)
+
+    # Plot confusion matrix
+    cm = confusion_matrix(y_true=data[TARGETS][:examples_limit], y_pred=preds)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target_names)
+    disp = disp.plot(include_values=True, cmap='viridis', ax=None, xticks_rotation='horizontal')
+    plt.show()
+    
+    #plot_confusion_matrix(cm=cm, classes=target_names, title='', normalize=False, cmap='viridis')
+    
+    #plot_confusion_matrix(model, data[FEATURES][:examples_limit], data[TARGETS][:examples_limit], display_labels=CLASS_NAMES)
 
     # Print the results
-    print('****Evaluation Report****')
-    print('Loss: {}\nAccuracy: {}\n'.format(loss, acc))
-    print('Classification Report:\n')
+    print("\nClassification Report for: {}\n".format(name))
+    print('Loss: {}, Accuracy: {}\n'.format(loss, acc))
     print(report)
 
+#def plot_confusion_matrix(cm, classes,
+#                        normalize=False,
+#                        title='Confusion matrix',
+#                        cmap=plt.cm.Blues):
+#    """
+#    This function prints and plots the confusion matrix.
+#    Normalization can be applied by setting `normalize=True`.
+#    """
+#    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+#    plt.title(title)
+#    plt.colorbar()
+#    tick_marks = np.arange(len(classes))
+#    plt.xticks(tick_marks, classes, rotation=45)
+#    plt.yticks(tick_marks, classes)
+#
+#    if normalize:
+#        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+#        print("Normalized confusion matrix")
+#    else:
+#        print('Confusion matrix, without normalization')
+#
+#    print(cm)
+#
+#    thresh = cm.max() / 2.
+#    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+#        plt.text(j, i, cm[i, j],
+#            horizontalalignment="center",
+#            color="white" if cm[i, j] > thresh else "black")
+#
+#    plt.tight_layout()
+#    plt.ylabel('True label')
+#    plt.xlabel('Predicted label')
 
 if __name__ == '__main__':
     main()
