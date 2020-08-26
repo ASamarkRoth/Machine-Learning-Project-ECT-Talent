f383bee37ea5cdd5651aac21151e29c5d74caa87
Anton Såmark-Roth <anton.samarkroth@gmail.com>
2020-08-26 14:20:28 +0200

    FCNNTraining successful. Started Report.

--- fcnn/eval.py
+++ fcnn/eval.py
@@ -7,8 +7,13 @@ import numpy as np
 import tensorflow as tf
 from scipy import sparse
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import itertools
 
-from utils.data import CLASS_NAMES, load_discretized_data
+from data_processing.data import CLASS_NAMES, load_discretized_data
 
 FEATURES = 0
 TARGETS = 1
@@ -28,6 +33,9 @@ TARGETS = 1
               help='Limit on the number of examples to use during testing.')
 @click.option('--seed', type=click.INT, default=71, nargs=1, help='Random seed.')
 def main(model_file, data_dir, data_combine, prefix, binary, examples_limit, seed):
+    eval(model_file, data, data_combine, binary, examples_limit, seed, reverse_labels)
+
+def eval(model_file, data, name='CNN', data_combine=False, binary=True, examples_limit=-1, seed=71, reverse_labels=False):
     """This script will evaluate an FCNN classifier.
 
     Loss, accuracy, and classification metrics are printed to the console.
@@ -36,43 +44,46 @@ def main(model_file, data_dir, data_combine, prefix, binary, examples_limit, see
 
     # Set random seeds
     np.random.seed(seed)
-    tf.set_random_seed(seed)
+    tf.random.set_seed(seed)
 
-    # Load data
-    if data_combine:
-        a, b = load_discretized_data(data_dir, prefix, categorical=True, binary=binary)
-        test = sparse.vstack([a[FEATURES], b[FEATURES]]), np.concatenate([a[TARGETS], b[TARGETS]], axis=0)
-    else:
-        _, test = load_discretized_data(data_dir, prefix, categorical=True, binary=binary)
+    ## Load data
+    #if data_combine:
+    #    a, b = load_discretized_data(data_dir, prefix, categorical=True, binary=binary)
+    #    test = sparse.vstack([a[FEATURES], b[FEATURES]]), np.concatenate([a[TARGETS], b[TARGETS]], axis=0)
+    #else:
+    #    _, test = load_discretized_data(data_dir, prefix, categorical=True, binary=binary)
 
     if examples_limit == -1:
-        examples_limit = test[TARGETS].shape[0]
+        examples_limit = data[TARGETS].shape[0]
 
     # Load the model
     model = tf.keras.models.load_model(model_file)
 
     # Evaluate the model
-    loss, acc = model.evaluate(test[FEATURES][:examples_limit], test[TARGETS][:examples_limit])
+    loss, acc = model.evaluate(data[FEATURES][:examples_limit], data[TARGETS][:examples_limit])
 
     # Make predictions
-    preds = np.argmax(model.predict(test[FEATURES][:examples_limit]), axis=1)
+    preds = np.round(model.predict(data[FEATURES][:examples_limit]))
+    target_names = CLASS_NAMES
 
-    if binary:
-        target_names = [CLASS_NAMES[0], 'non-' + CLASS_NAMES[0]]
-    else:
-        target_names = CLASS_NAMES
+    #if binary:
+    #    target_names = [CLASS_NAMES[0], 'non-' + CLASS_NAMES[0]]
+    #else:
+    #    target_names = CLASS_NAMES
 
     # Get classification metrics
-    report = classification_report(np.argmax(test[TARGETS][:examples_limit], axis=1), preds,
-                                   target_names=target_names,
-                                   digits=2)
+    report = classification_report(data[TARGETS][:examples_limit], preds, target_names=target_names, digits=2)
+
+    # Plot confusion matrix
+    cm = confusion_matrix(y_true=data[TARGETS][:examples_limit], y_pred=preds)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target_names)
+    disp = disp.plot(include_values=True, cmap='viridis', ax=None, xticks_rotation='horizontal')
+    plt.show()
 
     # Print the results
-    print('****Evaluation Report****')
-    print('Loss: {}\nAccuracy: {}\n'.format(loss, acc))
-    print('Classification Report:\n')
+    print("\nClassification Report for: {}\n".format(name))
+    print('Loss: {}, Accuracy: {}\n'.format(loss, acc))
     print(report)
-
 
 if __name__ == '__main__':
     main()
--- fcnn/train.py
+++ fcnn/train.py
@@ -3,13 +3,17 @@
 Author: Ryan Strauss
 """
 
+import datetime
+import json
+import sys
+
 import click
 import numpy as np
 import os
 import tensorflow as tf
 from sklearn.utils.class_weight import compute_class_weight
 
-from utils.data import load_discretized_data
+from data_processing.data import load_discretized_data
 
 FEATURES = 0
 TARGETS = 1
@@ -36,8 +40,10 @@ TARGETS = 1
 @click.option('--examples_limit', type=click.INT, default=-1, nargs=1,
               help='Limit on the number of training examples to use during training.')
 @click.option('--seed', type=click.INT, default=71, nargs=1, help='Random seed.')
-def main(data_dir, log_dir, hidden_layers, prefix, epochs, batch_size, rebalance, binary, dropout, lr, decay,
-         validation_split, examples_limit, seed):
+def main(data_dir, log_dir, hidden_layers, prefix, epochs, batch_size, rebalance, binary, dropout, lr, decay, validation_split, examples_limit, seed):
+    train(data_dir, log_dir, hidden_layers, prefix, epochs, batch_size, rebalance, binary, dropout, lr, decay, validation_split, examples_limit, seed)
+
+def train(train, log_dir, hidden_layers, prefix='', epochs=10, batch_size=32, rebalance=False, binary=True, use_dropout=False, dropout=0.5, lr=1e-4, decay=0., validation_split=0.15, examples_limit=-1, seed=71):
     """This script will train a FCNN classifier."""
     assert 0 < validation_split < 1, 'validation_split must be in range (0, 1)'
 
@@ -46,35 +52,47 @@ def main(data_dir, log_dir, hidden_layers, prefix, epochs, batch_size, rebalance
 
     # Set random seeds
     np.random.seed(seed)
-    tf.set_random_seed(seed)
+    tf.random.set_seed(seed)
 
     # Load data
-    train, _ = load_discretized_data(data_dir, prefix=prefix, categorical=True, binary=binary)
+    #train, _ = load_discretized_data(data_dir, prefix=prefix, categorical=True, binary=binary)
 
-    num_categories = train[TARGETS].shape[1]
-
-    if examples_limit == -1:
-        examples_limit = train[TARGETS].shape[0]
+    #num_categories = train[TARGETS].shape[1]
+    num_categories = 1
 
     # Build model
     model = tf.keras.Sequential()
 
     model.add(tf.keras.layers.Dense(hidden_layers[0], input_dim=train[FEATURES].shape[1], activation='relu'))
-    model.add(tf.keras.layers.Dropout(dropout))
+    if use_dropout:
+        model.add(tf.keras.layers.Dropout(dropout))
     for neurons in hidden_layers[1:]:
         model.add(tf.keras.layers.Dense(neurons, input_dim=train[FEATURES].shape[1], activation='relu'))
-        model.add(tf.keras.layers.Dropout(dropout))
-    model.add(tf.keras.layers.Dense(num_categories, activation='softmax'))
+        if use_dropout:
+            model.add(tf.keras.layers.Dropout(dropout))
+    model.add(tf.keras.layers.Dense(num_categories, activation='sigmoid'))
 
     opt = tf.keras.optimizers.Adam(lr=lr, decay=decay)
-    loss = 'binary_crossentropy' if num_categories == 2 else 'categorical_crossentropy'
+    loss = 'binary_crossentropy'# if num_categories == 2 else 'categorical_crossentropy'
+
+    #print("Loss:", loss)
 
     model.compile(loss=loss,
                   optimizer=opt,
                   metrics=['accuracy'])
 
-    os.makedirs(os.path.join(log_dir, 'ckpt'))
-    ckpt_path = os.path.join(log_dir, 'ckpt', 'epoch-{epoch:02d}.h5')
+    print(model.summary())
+    #os.makedirs(os.path.join(log_dir, 'ckpt'), exist_ok=True)
+    #ckpt_path = os.path.join(log_dir, 'ckpt', 'epoch-{epoch:02d}.h5')
+    
+    log_run = "nodes{}_dropout{}_lr{}_decay{}_samples{}".format(hidden_layers[0], use_dropout, lr, decay, examples_limit)
+    #print("Log for run:", log_run)
+    
+    log_dir = os.path.join(log_dir, log_run, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    os.makedirs(log_dir, exist_ok=True)
+    print("\nWriting fits to:", log_dir)
+    ckpt_path = os.path.join(log_dir, 'epoch-{epoch:02d}.h5')
+    print("Checkpoint path:", ckpt_path)
 
     # Get class weights
     if rebalance:
@@ -87,22 +105,51 @@ def main(data_dir, log_dir, hidden_layers, prefix, epochs, batch_size, rebalance
     # Setup checkpoint callback
     ckpt_callback = tf.keras.callbacks.ModelCheckpoint(ckpt_path,
                                                        save_weights_only=False,
-                                                       period=1,
+                                                       save_frequency=1,
                                                        save_best_only=False,
                                                        monitor='val_loss')
 
     # Setup TensorBoard callback
     tb_callback = tf.keras.callbacks.TensorBoard(log_dir)
 
+    if examples_limit == -1:
+        examples_limit = train[TARGETS].shape[0]
+
+    if examples_limit > train[TARGETS].shape[0]:
+        warnings.warn('`examples_limit` is larger than the number of examples in the training set. The entire training '
+                      'set will be used.')
+        examples_limit = train[TARGETS].shape[0]
+
     # Train the model
-    model.fit(train[FEATURES][:examples_limit],
-              train[TARGETS][:examples_limit],
-              epochs=epochs,
-              batch_size=batch_size,
-              validation_split=validation_split,
-              verbose=1,
-              class_weight=class_weight,
-              callbacks=[tb_callback, ckpt_callback])
+    train_start_time = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+
+    history = model.fit(train[FEATURES][:examples_limit],
+                        train[TARGETS][:examples_limit],
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_split=validation_split,
+                        verbose=1,
+                        class_weight=class_weight,
+                        callbacks=[tb_callback, ckpt_callback])
+
+    train_end_time = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+
+    history_filename = os.path.join(log_dir, 'history.json')
+    info_filename = os.path.join(log_dir, 'info.txt')
+    
+    model_filename = os.path.join(log_dir, 'saved_model.h5')
+    model.save(model_filename)
+
+    with open(history_filename, 'w') as file:
+        json.dump(history.history, file)
+
+    with open(info_filename, 'w') as file:
+        file.write('***Training Info***\n')
+        file.write('Training Start: {}'.format(train_start_time))
+        file.write('Training End: {}\n'.format(train_end_time))
+        file.write('Arguments:\n')
+        for arg in sys.argv:
+            file.write('\t{}\n'.format(arg))
 
 
 if __name__ == '__main__':
